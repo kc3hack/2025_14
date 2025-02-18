d5d271ed9dafbf2f42b43832ebf9b71719cb70f0
Kawa_ <yuyux0910@gmail.com>
2025-02-18 15:00:58 +0900

    [add] DBへのデータの登録を実装
    getは１データのみの取得となっており，未完成．
    エラー処理の実装などの課題はまだ残っている

--- backend/blueprint/collection.py
+++ backend/blueprint/collection.py
@@ -1,25 +1,50 @@
+from extensions import db
+from auth.models import User, Image
 from flask import Blueprint
 from flask import request, make_response, jsonify
+from datetime import datetime
 
 collection = Blueprint('collection', __name__)
 
-@collection.route("/collection", methods=['GET', 'POST'])
+@collection.route("/get", methods=['GET', 'POST'])
 def get():
     # フロントからjson形式でデータを受け取る
     data = request.get_json()
     # 受け取ったデータからID情報を抽出する
-    id = data['id']
+    user_id = data['user_id']
 
-    # 仮で結果をIDとする
-    res = id
-    # 結果の応答を辞書で作成
-    response = {'result': res}
+    # # 仮で結果をIDとする
+    # res = id
+    # # 結果の応答を辞書で作成
+    # response = {'result': res}
 
+    # 仮でDBのImageモデルのデータを全て検索して取得する
+    response = Image.query.all()
+    item = response[0]
+    d = {'user_id':item.user_id, 'image_path':item.image_path, 'datetime':item.datetime}
     # 辞書をjson形式として結果を返す
-    return make_response(jsonify(response))
+    return make_response(jsonify(d))
 
-@collection.route("/save", methods=['GET'])
+@collection.route("/save", methods=['POST'])
 def save():
+    # フロントからjson形式でデータを受け取る
+    data = request.get_json()
+
+    # 受け取ったデータから保存するデータを取得
+    user_id = data['user_id']
+    image_path = data['image_path']
+    datetime_str = data['datetime']
+    datetime_obj = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M:%S")
+
+    user = User(user_name="kawa", password_hash="hash", datetime=datetime_obj)
+    db.session.add(user)
+    db.session.commit()
+
+    # Imageクラスでインスタンス化
+    image = Image(user_id=user_id, image_path=image_path, datetime=datetime_obj)
+    db.session.add(image)
+    db.session.commit()
+
     return 'save'
 
 @collection.route("/delete", methods=['GET'])

d0323fee227cd2d4bb31722fa6cd2db31afe997a
KiCG <kishi.ryusei69@gmail.com>
2025-02-19 23:04:34 +0900

    タグ付け機能の使われていないルートを削除

--- backend/blueprint/gemini.py
+++ backend/blueprint/gemini.py
@@ -8,51 +8,6 @@ app = Blueprint('gemini', __name__)
 
 UPLOAD_FOLDER = 'pictures'
 
-@app.route("/prosess_image", methods=["POST"]) #画像を受け取った場合に動作する
-def upload_file():
-    if 'file' not in request.files:
-        return "No file part", 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file", 400
-
-    if file:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(file_path)
-        result, tag_result = read_image(file_path) #画像パスを投げるとgeminiからテキスト形式で返される
-
-        datetime_obj = datetime.now() #現在の時間の取得
-        existing_tag = Tag.query.filter_by(tag=tag_result).first()# 最初の一致するレコードを取得
-        if existing_tag: #すでにデータベースにタグが存在する場合
-            return jsonify({"result": result, "image_path": file_path, "tag_name": tag_result, "ID": existing_tag.id}) #geminiの出力結果と画像パス、タグの名前をjson形式で返す
-        else:
-            datetime_obj = datetime.now() #現在の時間の取得
-            new_tag = Tag(tag=tag_result, datetime=datetime_obj)
-            Tag.session.add(new_tag)  # セッションに新しいタグを追加
-            Tag.session.commit()  # 変更をコミットしてデータベースに保存
-            return jsonify({"result": result, "image_path": file_path, "tag_name": tag_result, "ID": new_tag.id}) #geminiの出力結果と画像パス、タグの名前をjson形式で返す
-
-@app.route("/process_text", methods=["POST"]) #テキストを受け取った場合に動作する
-def process_uploaded_text():
-    data = request.json
-    if not data or 'text' not in data:
-        return "No text provided", 400
-
-    text = data['text']
-    result, tag_result = read_text(text) #テキストを投げるとgeminiからテキスト形式で返される
-
-    existing_tag = Tag.query.filter_by(tag=tag_result).first()# 最初の一致するレコードを取得
-    if existing_tag: #すでにデータベースにタグが存在する場合
-        return jsonify({"result": result, "tag_name": tag_result, "ID": existing_tag.id}) #geminiの出力結果とタグの名前をjson形式で返す
-    else: #データベースにタグが存在しない場合
-        datetime_obj = datetime.now() #現在の時間の取得
-        new_tag = Tag(tag=tag_result, datetime=datetime_obj)
-        Tag.session.add(new_tag)  # セッションに新しいタグを追加
-        Tag.session.commit()  # 変更をコミットしてデータベースに保存
-        return jsonify({"result": result, "tag_name": tag_result, "ID": new_tag.id}) #geminiの出力結果とタグの名前をjson形式で返す
-
-#upload_file()とprocess_uploaded_text()の処理を一つにまとめる
 @app.route("/process", methods=["POST"])  # 画像もテキストも処理するエンドポイント
 def process_uploaded_data():
     # 画像のアップロードチェック

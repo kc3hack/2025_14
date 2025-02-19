d2f8ed8e43e84bb9ac754ca888aa496ffd609ed1
solle458 <show04go@icloud.com>
2025-02-19 13:42:59 +0900

    add Tag

--- backend/models/text.py
+++ backend/models/text.py
@@ -4,7 +4,9 @@ class Text(db.Model):
     __tablename__ = "text"
     text_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("profile.user_id"))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tag.tag_id"))
     text = db.Column(db.String)
     datetime = db.Column(db.DateTime)
 
     user = db.relationship("User", backref="texts")
+    tag = db.relationship("Tag", backref="texts")

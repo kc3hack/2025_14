2643cdd02de612cbc98efef424b99c90ae4d2825
KiCG <kishi.ryusei69@gmail.com>
2025-02-19 19:02:51 +0900

    データベースへのタグの追加

--- backend/blueprint/gemini.py
+++ backend/blueprint/gemini.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 import os
 from gemini.think import read_image, read_text, daily_lucky_powder
+from models import Tag
 
 app = Blueprint('gemini', __name__)
 
@@ -18,8 +19,16 @@ def upload_file():
     if file:
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(file_path)
-        result, tag = read_image(file_path) #画像パスを投げるとgeminiからテキスト形式で返される
-        return jsonify({"result": result, "image_path": file_path, "tag_name": tag}) #geminiの出力結果と画像パス、タグの名前をjson形式で返す
+        result, tag_result = read_image(file_path) #画像パスを投げるとgeminiからテキスト形式で返される
+
+        existing_tag = Tag.query.filter_by(tag=tag_result).first()# 最初の一致するレコードを取得
+        if existing_tag: #すでにデータベースにタグが存在する場合
+            return jsonify({"result": result, "image_path": file_path, "tag_name": tag_result, "ID": existing_tag.id}) #geminiの出力結果と画像パス、タグの名前をjson形式で返す
+        else:
+            new_tag = Tag(tag=tag_result)
+            Tag.session.add(new_tag)  # セッションに新しいタグを追加
+            Tag.session.commit()  # 変更をコミットしてデータベースに保存
+            return jsonify({"result": result, "image_path": file_path, "tag_name": tag_result, "ID": new_tag.id}) #geminiの出力結果と画像パス、タグの名前をjson形式で返す
 
 @app.route("/process_text", methods=["POST"]) #テキストを受け取った場合に動作する
 def process_uploaded_text():
@@ -28,8 +37,16 @@ def process_uploaded_text():
         return "No text provided", 400
 
     text = data['text']
-    result, tag = read_text(text) #テキストを投げるとgeminiからテキスト形式で返される
-    return jsonify({"result": result, "tag_name": tag}) #geminiの出力結果とタグの名前をjson形式で返す
+    result, tag_result = read_text(text) #テキストを投げるとgeminiからテキスト形式で返される
+
+    existing_tag = Tag.query.filter_by(tag=tag_result).first()# 最初の一致するレコードを取得
+    if existing_tag: #すでにデータベースにタグが存在する場合
+        return jsonify({"result": result, "tag_name": tag_result, "ID": existing_tag.id}) #geminiの出力結果とタグの名前をjson形式で返す
+    else: #データベースにタグが存在しない場合
+        new_tag = Tag(tag=tag_result)
+        Tag.session.add(new_tag)  # セッションに新しいタグを追加
+        Tag.session.commit()  # 変更をコミットしてデータベースに保存
+        return jsonify({"result": result, "tag_name": tag_result, "ID": new_tag.id}) #geminiの出力結果とタグの名前をjson形式で返す
 
 @app.route("/daily_lucky_powder", methods=["GET"]) #占いを行う際に動作する
 def get_daily_lucky_powder():

b6dce0cb74674500f9ffd1817ab80b9ffd40f39f
Kawa_ <yuyux0910@gmail.com>
2025-02-20 18:01:41 +0900

    [add] 画像のパスではなく名前を保存する

--- backend/blueprint/collection.py
+++ backend/blueprint/collection.py
@@ -46,19 +46,19 @@ def save():
     data = request.get_json()
 
     # dataに必要な形式がないならエラー
-    if not data or "user_id" not in data or "image_path" not in data or "caption" not in data or "tag_id" not in data:
+    if not data or "user_id" not in data or "image_name" not in data or "caption" not in data or "tag_id" not in data:
         return "Not enough parameter", 400
 
     # 受け取ったデータから保存するデータを取得
     user_id = data["user_id"]
-    image_path = data["image_path"]
+    image_name = data["image_name"]
     caption = data["caption"]
     tag_id = data["tag_id"]
     # 現在時刻を取得
     datetime_obj = datetime.now()
 
     # Imageクラスでデータをインスタンス化
-    image_data = Image(user_id=user_id, image_path=image_path, caption=caption, tag_id=tag_id, datetime=datetime_obj)
+    image_data = Image(user_id=user_id, image_name=image_name, caption=caption, tag_id=tag_id, datetime=datetime_obj)
     db.session.add(image_data)  # ImageをDBに追加
     db.session.commit()         # 追加を適用
 

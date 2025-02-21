bb373ff8c402fdf710c368f34f00120c86cc5400
solle458 <show04go@icloud.com>
2025-02-21 22:09:13 +0900

    user_id削除

--- backend/collection/collection_get.py
+++ backend/collection/collection_get.py
@@ -1,19 +1,17 @@
 from flask import session, make_response, jsonify
-from models import Image, Tag
+from models import Image, Tag, User
 
 def get(data):
 
     if 'user_name' not in session:
         return f'Logged in as {session["user_name"]}', 400
 
-    if not data or "user_id" not in data: # dataに必要な形式がないならエラー
-        return "Not enough parameter", 400
-
     # ID情報を抽出する
-    user_id = data["user_id"]
+    user_name = session['user_name']
+    user = User.query.filter_by(user_name=user_name).first()
 
     # DBのImageモデルからuser_idで絞り込んでデータを取得
-    response = Image.query.filter_by(user_id=user_id)
+    response = Image.query.filter_by(user_id=user.user_id)
 
     result_list = [] # 検索結果を保存するリスト
     for res in response:
--- backend/collection/collection_save.py
+++ backend/collection/collection_save.py
@@ -1,5 +1,5 @@
 from flask import session, make_response, jsonify
-from models import Image, Tag
+from models import Image, User
 from datetime import datetime
 
 def save(data):
@@ -9,11 +9,13 @@ def save(data):
         return f'Logged in as {session["user_name"]}', 400
 
     # dataに必要な形式がないならエラー
-    if not data or "user_id" not in data or "image_path" not in data or "caption" not in data or "tag_id" not in data:
+    if not data or "image_path" not in data or "caption" not in data or "tag_id" not in data:
         return "Not enough parameter", 400
 
     # 受け取ったデータから保存するデータを取得
-    user_id = data["user_id"]
+    user_name = session['user_name']
+    user = User.query.filter_by(user_name=user_name).first()
+    user_id = user.user_id
     image_path = data["image_path"]
     caption = data["caption"]
     tag_id = data["tag_id"]
@@ -25,4 +27,4 @@ def save(data):
     db.session.add(image_data)  # ImageをDBに追加
     db.session.commit()         # 追加を適用
 
-    return "success"
+    return jsonify({"status": "success"}), 200

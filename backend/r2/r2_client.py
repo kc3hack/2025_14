d2428306857acf261fb76c707c729f8da77f6768
solle458 <show04go@icloud.com>
2025-02-21 16:05:15 +0900

    修正

--- backend/r2/r2_client.py
+++ backend/r2/r2_client.py
@@ -9,7 +9,6 @@ R2_ENDPOINT_URL = os.getenv("R2_ENDPOINT_URL")
 R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
 R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
 R2_BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
-
 # `s3v4` を明示的に設定
 config = Config(signature_version="s3v4")
 

aac882d53337318d3f7e57ee3a3a7965575fd7d9
KiCG <kishi.ryusei69@gmail.com>
2025-02-22 16:35:49 +0900

    占いのui(挫折)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./HomeScreen/HomeScreen";
 import OutputScreen from "./OutputScreen/OutputScreen";
+import LuckyPowder from "./lucky_powder/lucky_powder"
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OutputScreen />} />
+        <Route path="/" element={<LuckyPowder />} />
         <Route path="/OutputScreen" element={<HomeScreen />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/lucky_powder/lucky_powder.js
+++ frontend/src/lucky_powder/lucky_powder.js
@@ -1,36 +1,42 @@
 import React from "react";
-import Button from "./material/Button.png";
-import Background from "./material/background.png";
-import avatar from "./material/3davatars.png";
-import fortune from "./material/daily_lucky_powder.png";
-import "./signin.css"
+import Button from "./material/fortune.png";
+import avatar from "./material/avatar.png";
+import text from "./material/lucky.png";
 
-function SingInButtonImage() {
-  return <img src={Button} alt="Button" />;
+function FortuneButton() {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  return (
+    <button className="button" onClick={handleClick}>
+      <img src={Button} alt="Button" />
+    </button>
+  );
 }
 
-function LogoImage() {
+function AvatarImage() {
   return (
     <div className="logo-image">
-      <img src={Background} alt="Logo" />
-    </div>);
+      <img src={avatar} alt="avatar" />
+    </div>
+  );
+}
+
+function TextImage() {
+  return (
+    <div className="logo-image">
+      <img src={text} alt="text" />
+    </div>
+  );
 }
 
 export default function SignIn() {
   return (
-    <div>
-      <div>
-        <LogoImage></LogoImage>
-      </div>
-
-      <div>
-        <SingInButtonImage></SingInButtonImage>
-      </div>
-      <div className="text-center mt-32">
-        <a href="/" className="[font-family:'Yuji_Mai-Regular',Helvetica] text-[45.3px] leading-[68px] text-[#04abff] underline">
-          サインアップ
-        </a>
-      </div>
+    <div className="container">
+      <TextImage />
+      <AvatarImage />
+      <FortuneButton />
     </div>
   );
 }

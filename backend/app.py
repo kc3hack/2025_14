95bf43eab7d1f45e4fe4efc6b7e0eb9796a951f8
solle458 <139096138+solle458@users.noreply.github.com>
2025-02-23 15:25:48 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,22 +5,28 @@ from flask_session import Session
 from config import config
 from db_instance import db
 import os
+from datetime import timedelta
 
 def create_app(config_key):
     app = Flask(__name__)
     app.secret_key = os.getenv('AUTH_SECRET_KEY')
     app.config.from_object(config[config_key])
-    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config["SECRET_KEY"] = os.getenv("AUTH_SECRET_KEY")
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SESSION_PERMANENT"] = timedelta(days=7)
+    app.config["SESSION_COOKIE_HTTPONLY"] = True
+    # app.config["SESSION_COOKIE_SAMESITE"] = 'Lax'
+    app.config["SESSION_COOKIE_HTTPONLY"] = False
+    app.config["SESSION_COOKIE_DOMAIN"] = None
     Session(app)
     db.init_app(app)
     Migrate(app, db)
-    CORS(app, origins=["http://localhost:3000"], supports_credentials=True)
+    CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
 
-    from blueprint import auth,gemini
+    from blueprint import auth, gemini
     from blueprint import collection
     app.register_blueprint(auth.auth)
     app.register_blueprint(collection.collection, url_prefix="/collection")
-
     app.register_blueprint(gemini.app)
     return app
 

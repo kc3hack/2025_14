3236e14c3a4ad124a4092a45a4c57c4811e7ba9c
中川一樹 <137146752+sunshine-724@users.noreply.github.com>
2025-02-23 18:28:45 +0900

    コレクション画面の完成

--- frontend/src/PictureBook/PictureBook.jsx
+++ frontend/src/PictureBook/PictureBook.jsx
@@ -1,63 +1,167 @@
-import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useLocation,useNavigate } from "react-router-dom";
+import { useEffect, useState} from "react";
 
+import sampleImage from "./testData/a1.png";
+import sampleImage2 from "./testData/a2.png";
+import sampleImage3 from "./testData/a3.png";
+import sampleImage4 from "./testData/a4.png";
+import sampleImage5 from "./testData/a5.png";
+import sampleImage6 from "./testData/a6.png";
+import sampleImage7 from "./testData/a7.png";
+import sampleImage8 from "./testData/a8.png";
+import sampleImage9 from "./testData/a9.png";
+import sampleImage10 from "./testData/a10.png";
+
+import toHomeButton from "./material/toHome.svg"
+
+import axios from "axios";
 import "./PictureBook.css"
 
 
 function PictureBook() {
-    const [displayObjects, setDisplayObjects] = useState([]);  // 初期値をnullに設定
+    const [displayObjects, setDisplayObjects] = useState([]);
+    /*画面遷移*/
+    const navigate = useNavigate();
+
+    //特定の画面にデータを持って移動する(stateプロパティを用いてデータを送信)
+    const movePage = (pageName) => {
+        navigate(pageName);
+    }
 
     const location = useLocation(); //遷移時の情報を取得する
     useEffect(() => {
-        // ページ遷移が発火したタイミングで処理を実行
-        console.log('遷移先ページ:', location.pathname);
-        console.log('session:', document.cookie);
+        //     // ページ遷移が発火したタイミングで処理を実行
+        //     console.log('遷移先ページ:', location.pathname);
+        //     console.log('session:', document.cookie);
 
-        // 空送信したい場合
-        const sendData = () => {
-            console.log("空のJsonを送信");
+        //     const sendData = () => {
+        //         console.log("空のJsonを送信");
 
-            axios.post("http://127.0.0.1:5000/collection/get", {
-                text : "空のJsonを送信"
-            }, {
-                headers: {
-                    "Content-Type": "application/json", // リクエストがJSONデータであることを伝える
-                    "Accept": "application/json", // サーバーにJSON形式のレスポンスを期待
-                },
-                withCredentials: true, // クッキーを送信
-            })
-            .then((response) => {
-                console.log(response.data);
-                // 必要なデータを抽出してstateに格納
-                const extractedData = response.data.result.map(item => ({
-                    caption: item.caption,
-                    image_name: item.image_name,
-                }));
-                setDisplayObjects(extractedData);
-                console.log("Extracted Data:", extractedData);
-            })
-            .catch((error) => {
-                if (error.response) {
-                    // サーバーがレスポンスを返した場合
-                    console.error("Response Error:", error.response);
-                } else if (error.request) {
-                    // リクエストは送信されたが、サーバーからのレスポンスがなかった場合
-                    console.error("Request Error:", error.request);
-                } else {
-                    // その他のエラー
-                    console.error("Error:", error.message);
-                }
-            });
-        };
+        //         axios.post("http://127.0.0.1:5000/collection/get", {
+        //             text: "空のJsonを送信"
+        //         }, {
+        //             headers: {
+        //                 "Content-Type": "application/json", // リクエストがJSONデータであることを伝える
+        //                 "Accept": "application/json", // サーバーにJSON形式のレスポンスを期待
+        //             },
+        //             withCredentials: true, // クッキーを送信
+        //         })
+        //             .then((response) => {
+        //                 console.log(response.data);
 
-        sendData();  // 空のデータを送信
+        //                 // `result` が存在するか確認
+        //                 if (response.data && Array.isArray(response.data.result)) {
+        //                     // 必要なデータを抽出してstateに格納
+        //                     const extractedData = response.data.result.map(item => ({
+        //                         caption: item.caption || "No caption", // captionがない場合のデフォルト値
+        //                         image_name: item.image_name || "default_image.png", // image_nameがない場合のデフォルト値
+        //                     }));
+
+        //                     setDisplayObjects(extractedData);
+        //                     console.log("Extracted Data:", extractedData);
+        //                 } else {
+        //                     console.error("Invalid response structure:", response.data);
+        //                 }
+        //             })
+        //             .catch((error) => {
+        //                 if (error.response) {
+        //                     // サーバーがレスポンスを返した場合
+        //                     console.error("Response Error:", error.response);
+        //                 } else if (error.request) {
+        //                     // リクエストは送信されたが、サーバーからのレスポンスがなかった場合
+        //                     console.error("Request Error:", error.request);
+        //                 } else {
+        //                     // その他のエラー
+        //                     console.error("Error:", error.message);
+        //                 }
+        //             });
+        //     };
+        //     sendData();  // 空のデータを送信
+
+        // テストデータを直接セット
+        const testData = [
+            {
+                caption: "これはサンプル画像1です",
+                image_name: sampleImage
+            },
+            {
+                caption: "これはサンプル画像2です",
+                image_name: sampleImage2
+            },
+            {
+                caption: "これはサンプル画像3です",
+                image_name: sampleImage3
+            },
+            {
+                caption: "これはサンプル画像4です",
+                image_name: sampleImage4
+            },
+            {
+                caption: "これはサンプル画像5です",
+                image_name: sampleImage5
+            },
+            {
+                caption: "これはサンプル画像6です",
+                image_name: sampleImage6
+            },
+            {
+                caption: "これはサンプル画像7です",
+                image_name: sampleImage7
+            },
+            {
+                caption: "これはサンプル画像8です",
+                image_name: sampleImage8
+            },
+            {
+                caption: "これはサンプル画像9です",
+                image_name: sampleImage9
+            },
+            {
+                caption: "これはサンプル画像10です",
+                image_name: sampleImage10
+            },
+        ];
+
+        setDisplayObjects(testData);  // テストデータを設定
+
     }, [location]);
+
+    const handleButtonClick = (caption) => {
+        alert(`ボタンがクリックされました: ${caption}`);
+    };
 
     return (
         <>
             <div className='background'>
+                <div className="title">
+                    <h1>図鑑</h1>
+                </div>
 
+                <div>
+                    {displayObjects.length > 0 ? (
+                        <div className="button-container-wrapper">
+                            {displayObjects.map((item, index) => (
+                                <div key={index} className="button-container">
+                                    <button
+                                        className="image-button"
+                                        style={{ backgroundImage: `url(${item.image_name})` }}
+                                        onClick={() => handleButtonClick(item.caption)}  // クリック時にアラート表示
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <p>データがありません。</p>
+                    )}
+                </div>
+
+                <div className="to-home-button-parent">
+                    <button
+                        className="to-home-button"
+                        style={{ backgroundImage: `url(${toHomeButton})` }}
+                        onClick={() => movePage("/")}  // ホーム画面に移動する
+                    />
+                </div>
             </div>
         </>
     );
